523f0f9d73ca2347d2c9b1a9b6b7038b74bced32
Max Lerman <mjl82601@gmail.com>
2023-02-12 00:26:25 +0000

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import Home from "./components/Home";
+import Game from "./components/Game";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
