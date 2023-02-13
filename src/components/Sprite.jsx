43a415c700fefa80537efa4abf0c728579544f19
Max Lerman <mjl82601@gmail.com>
2023-02-13 15:51:12 +0000

    updated Sprite.jsx

--- src/components/Sprite.jsx
+++ src/components/Sprite.jsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Sprite = ({
   position,
   imageSrc,
   scale = 1,
   maxFrames = 1,
+  frameRate = 11,
   offset = {
     x: 0,
     y: 0,
   },
-  usableCanvasHeight,
-  topResetValue,
+  usableCanvasHeight, // y value to reference the bottom of a usable background
+  topResetValue, // y value to reset the position of the fighter sprites to
 }) => {
-
+ const [width, setWidth] = useState(50);
+ const [height, setHeight] = useState(150);
+ const image = new Image();
+ image.src = imageSrc;
+ const [currentFrame, setCurrentFrame] = useState(0);
+ const [elapsedFrames, setElapsedFrames] = useState(0);
 };
