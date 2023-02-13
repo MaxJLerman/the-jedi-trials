d1939f6fa9ef9a48c7c90ab93c87c7fb6c9bbfe9
Max Lerman <mjl82601@gmail.com>
2023-02-13 11:48:35 +0000

    started converting Sprite class to JSX

---
+++ src/components/Sprite.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Sprite = ({
+  position,
+  imageSrc,
+  scale = 1,
+  maxFrames = 1,
+  offset = {
+    x: 0,
+    y: 0,
+  },
+  usableCanvasHeight,
+  topResetValue,
+}) => {
+
+};
