5608f923b4c214a1fd41c8af39b75643fd94ef5e
Max Lerman <mjl82601@gmail.com>
2023-02-22 17:31:20 +0000

    started implementing background

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 
 import {OutermostDiv, SpritesContainer, TimerContainer, ResultContainer, PlayerHealthBarContainer, EnemyHealthBarContainer, HealthBarForeground, EnemyHealthBarForeground, HealthBarBackground} from "./styled-components";
 
 const Game = () => {
+  // prevents error: Uncaught TypeError: Cannot read properties of null (reading 'getContext')
+  useEffect(() => {
+   window.addEventListener('load', () => {
+      const canvas = document.getElementById('gameCanvas');
+      const context = canvas.getContext('2d');
+
+      canvas.width = 1024;
+      canvas.height = 576;
+    
+      context.fillRect(0, 0, canvas.width, canvas.height);
+   });
+  }, []);
+
+  
+
   return (
     <div>
       <h2>Game Page</h2>
@@ -30,7 +45,9 @@ const Game = () => {
           <ResultContainer>
             Tie
           </ResultContainer>
-          <canvas></canvas>
+          <canvas id="gameCanvas">
+
+          </canvas>
         </OutermostDiv>
         
         {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js" integrity="sha512-f8mwTB+Bs8a5c46DEm7HQLcJuHMBaH/UFlcgyetMqqkvTcYg4g5VXsYR71b3qC82lZytjNYvBj2pf0VekA9/FQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
